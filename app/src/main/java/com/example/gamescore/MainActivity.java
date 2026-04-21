a38a16f363d61d87b8d80df2a12bc50406457120
user-nick-nn <nazarenkonikitawow@gmail.com>
2026-04-21 07:45:09 +0300

    5.2 Методы ЖЦ onCreate и onDestroy

--- app/src/main/java/com/example/gamescore/MainActivity.java
+++ app/src/main/java/com/example/gamescore/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -26,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        if (savedInstanceState != null) {
+            score1 = savedInstanceState.getInt("score1");
+            score2 = savedInstanceState.getInt("score2");
+        }
+
         TextView textViewScoreTeam1 = findViewById(R.id.TextViewScoreTeam1);
         TextView textViewScoreTeam2 = findViewById(R.id.TextViewScoreTeam2);
 
@@ -45,5 +51,17 @@ public class MainActivity extends AppCompatActivity {
                 textViewScoreTeam2.setText(String.valueOf(++score2));
             }
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("score1", score1);
+        outState.putInt("score2", score2);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
